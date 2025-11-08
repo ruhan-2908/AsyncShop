7dc53fcd4fc5fdf253c459c4a96fd88d0ecd0cbe
ruhan-2908 <ruhankb29@gmail.com>
2025-11-08 22:59:41 +0530

    Add Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
-import { 
-  BrowserRouter as Router, 
-  Routes, 
-  Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Header/>
       <Router>
-        <div>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path='/search' element={<Home/>}/>
         </Routes>
-        </div>
+        <Footer />
       </Router>
-      <Footer/>
     </div>
   );
 }
