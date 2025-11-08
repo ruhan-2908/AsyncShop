020fef2063f59524f5595feebd039bc99312e153
ruhan-2908 <ruhankb29@gmail.com>
2025-11-08 19:59:18 +0530

    Add Order API

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,8 @@ const orders = require("./routes/order");
 const { connect } = require('http2');
 
 connectDatabase();
+
+app.use(express.json())
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,16 @@
-exports.createOrder = (req,res,next) => {
+const orderModel = require('../models/orderModel')
+
+
+
+//Create Order - /api/v1/order
+exports.createOrder = async (req,res,next) => {
+    const cartItems = req.body;
+    const amount = Number(cartItems.reduce((acc,item) =>(acc +  item.product.price * item.qty),0)).toFixed(2);
+    const status = 'Pending';
+    console.log("Amount " + amount)
+    const order = await orderModel.create({cartItems,amount,status})
     res.json({
         success :true,
-        message : "Order Works!"
+        order
     })
 }
