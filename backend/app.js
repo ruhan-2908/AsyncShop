97c53bc9ebbc971a2791a8952b34106420b3624e
ruhan-2908 <ruhankb29@gmail.com>
2025-11-08 13:33:50 +0530

    Add Modules and MongoDB connection:

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
+const connectDatabase = require("./config/connectDatabase")
 dotenv.config({path: path.join(__dirname,'config','config.env')});
 
 const products = require('./routes/product');
 const orders = require("./routes/order");
+const { connect } = require('http2');
+
+connectDatabase();
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
